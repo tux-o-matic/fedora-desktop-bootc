52034f3aced41b5f463ba5faca9d0d07f60edeba
Ben <tux-o-matic@users.noreply.github.com>
2025-12-15 13:37:29 +0100

    Moved flatpak apps install in CentOS Stream flavor to firstboot. Clean up more log files in image.

---
+++ centos/flatpaks-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+flatpak install -y --noninteractive flathub org.mozilla.firefox
+flatpak install -y --noninteractive flathub org.gimp.GIMP
+flatpak install -y --noninteractive flathub org.libreoffice.LibreOffice
+flatpak install -y --noninteractive flathub com.vscodium.codium 
