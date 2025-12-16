541cea12d16e5a42ee97745a92b5207eb6d0ba5b
Ben <tux-o-matic@users.noreply.github.com>
2025-12-16 14:40:35 +0100

    Removed welcome screen on first boot. Added VLC Flatpak. Removed "flatpak" dnf install command on Fedora which is already installed.

--- centos/flatpaks-install.sh
+++ centos/flatpaks-install.sh
@@ -3,4 +3,5 @@
 flatpak install -y --noninteractive flathub org.mozilla.firefox
 flatpak install -y --noninteractive flathub org.gimp.GIMP
 flatpak install -y --noninteractive flathub org.libreoffice.LibreOffice
+flatpak install -y --noninteractive flathub org.videolan.VLC
 flatpak install -y --noninteractive flathub com.vscodium.codium 
