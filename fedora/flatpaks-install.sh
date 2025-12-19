05208d2e2f20e4b5f2ac3aff749de04b36080fee
Ben <tux-o-matic@users.noreply.github.com>
2025-12-19 08:29:54 +0100

    Added Thunderbird to the list of Flatpak apps to install on first boot

--- fedora/flatpaks-install.sh
+++ fedora/flatpaks-install.sh
@@ -3,5 +3,6 @@
 flatpak install -y --noninteractive flathub org.mozilla.firefox
 flatpak install -y --noninteractive flathub org.gimp.GIMP
 flatpak install -y --noninteractive flathub org.libreoffice.LibreOffice
+flatpak install -y --noninteractive flathub org.mozilla.Thunderbird
 flatpak install -y --noninteractive flathub org.videolan.VLC
 flatpak install -y --noninteractive flathub com.vscodium.codium 
