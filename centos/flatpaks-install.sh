95a3f2d4fad47489ec7cc9c29a73f0e817662263
Ben <tux-o-matic@users.noreply.github.com>
2025-12-22 13:34:26 +0100

    Removed some simple Gnome multimedia apps that are replaced by feature rich apps from Flathub. Added Gnome extension to keep dash to dock. Timezone is now set on first boot based on current geo-location

--- centos/flatpaks-install.sh
+++ centos/flatpaks-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Set timezone based on location
+timedatectl set-timezone $(wget -qO - http://ip-api.com/line?fields=timezone)
+
+# Install Flatpak applications (Thunderbird is only available for x86_64)
 flatpak install -y --noninteractive flathub org.mozilla.firefox
 flatpak install -y --noninteractive flathub org.gimp.GIMP
 flatpak install -y --noninteractive flathub org.libreoffice.LibreOffice
--- fedora/flatpaks-install.sh
+++ fedora/flatpaks-install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+#Set timezone based on location
+timedatectl set-timezone $(wget -qO - http://ip-api.com/line?fields=timezone)
+
+# Install Flatpak applications (Thunderbird is only available for x86_64)
 flatpak install -y --noninteractive flathub org.mozilla.firefox
 flatpak install -y --noninteractive flathub org.gimp.GIMP
 flatpak install -y --noninteractive flathub org.libreoffice.LibreOffice
 flatpak install -y --noninteractive flathub org.mozilla.Thunderbird
+flatpak install -y --noninteractive flathub org.gnome.Rhythmbox3
 flatpak install -y --noninteractive flathub org.videolan.VLC
-flatpak install -y --noninteractive flathub com.vscodium.codium 
+flatpak install -y --noninteractive flathub com.vscodium.codium
